17c2613f7a7e021699f60a8b208cf9ab704f09c3
root <root@slayermx>
2026-07-05 06:37:25 +0200

    Fixed file headers

--- InitGui.py
+++ InitGui.py
@@ -1,11 +1,31 @@
-
+# *************************************************************************
+# *                                                                       *
+# * Copyright (c) 2026 BlackMaxMetal                                      *
+# *                                                                       *
+# * This program is free software; you can redistribute it and/or modify  *
+# * it under the terms of the GNU Lesser General Public License (LGPL)    *
+# * as published by the Free Software Foundation; either version 3 of     *
+# * the License, or (at your option) any later version.                   *
+# * for detail see the LICENCE text file.                                 *
+# *                                                                       *
+# * This program is distributed in the hope that it will be useful,       *
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# * GNU Library General Public License for more details.                  *
+# *                                                                       *
+# * You should have received a copy of the GNU Library General Public     *
+# * License along with this program; if not, write to the Free Software   *
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+# * USA                                                                   *
+# *                                                                       *
+# *************************************************************************
 
 import FreeCAD
 import FreeCADGui
 from PySide import QtCore
 
 def auto_start_multimonitor():
-    """Asynchronously injects and mounts the 4-panel window into FreeCAD's active thread."""
+
     try:
         # Dynamically import our custom multi-monitor module file from the Mod folder
         import MultiMonitor
